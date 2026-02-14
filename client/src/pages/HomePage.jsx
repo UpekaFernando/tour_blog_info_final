fe74dbf4a3c9b3b719b354896487fe9701765bbc
upeka fernando <upekakaushal@gmail.com>
2026-02-14 10:05:51 +0530

    changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -66,7 +66,7 @@ const HomePage = () => {
               textShadow: '2px 2px 6px rgba(0,0,0,0.7)'
             }}
           >
-            Discover Sri Lanka
+           lets  Discover Sri Lanka
           </Typography>
           <Typography 
             variant="h5" 
